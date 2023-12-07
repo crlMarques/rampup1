34fbfc493ce69a677af8a143dc8e4f3e0dfb634b
carlosm2 <carlosm2@vmware.com>
2023-12-07 12:16:48 -0300

    Running dish ordered

--- src/domain/client/ordered.ts
+++ src/domain/client/ordered.ts
@@ -7,41 +7,49 @@ import { Serializer } from "jsonapi-serializer";
 
 export async function createOrder(dishList: { dishId: number }[]) {
   const t = await sequelize.transaction();
-  const serializer = new Serializer('dishes', {
+  const ingredientSerializer = new Serializer('ingredients', {
+    attributes: ['id','quantity', 'dishId','stockId']
+  });
+  const dishSerializer = new Serializer('dishes', {
       attributes: ['name'],
       ingredients: {
         ref: 'id',
         attributes: ['dishId','stockId','quantity'],
-        included: false,
-        relationshipLinks: {
-          self: '/dishes/{id}/relationships/ingredients',
-          related: '/dishes/{id}/ingredients',
-        },
+        included: true
       }
     }); 
   try {
     let dishOrderList: {}[] = [];
+    let dishIngredientList: {dataValues: [Object]}[] = [];
     for (let dish of dishList) {
       let dishExist = await Dish.findOne({ where: { id: dish.dishId }, transaction: t })
       if (!dishExist) {
         throw new Error("Dishes not exists")
       }
       let dishesStock = await Dishstock.findAll({ where: { dishId: dish.dishId }, transaction: t })
-      if (dishesStock.length == 0) {
+      if (dishesStock.length == 0) {let dishOrderList: {}[] = [];
         throw new Error("Dish not exists in dishes_stock table")
       }
-      // for (let dishesObject of dishesStock) {
+       for (let dishesObject of dishesStock) {
       //   await Stock.decrement('quantity', {
       //     by: dishesObject.dataValues.quantity,
       //     where: { id: dishesObject.dataValues.stockId },
       //     transaction: t
       //   });
-      // }
+        dishIngredientList.push(dishesObject.dataValues)
+       }
       dishOrderList.push(dishExist)
-      dishOrderList.push(dishesStock)
+      
     }
-    console.log("Vendo a ordem:\n",dishOrderList)
-    return serializer.serialize(dishOrderList).data
+    console.log("\n\n", dishIngredientList )
+    console.log("\n\n", dishOrderList )
+    const serializedIngredients = ingredientSerializer.serialize(dishIngredientList);
+    console.log("Vendo o ingred serializer:\n",serializedIngredients)
+    const serializedDish = dishSerializer.serialize(dishOrderList);
+    console.log("Vendo o ingred serializerDish:\n",serializedDish)
+    serializedDish.included = serializedIngredients.data;
+    console.log("\n\n\nBora ver o serialize final: \n\n", serializedDish)
+    return serializedDish
   }catch (error: any) {
     await t.rollback();
     throw new Error(error.message)
